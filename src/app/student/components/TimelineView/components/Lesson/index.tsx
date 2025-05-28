7e226bd37eafa4dd52f6c09d0f3d7cbd4d6908c6
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-05-28 18:14:05 +0600

    Add lesson component

---
+++ src/app/student/components/TimelineView/components/Lesson/index.tsx
@@ -0,0 +1,77 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faClockFour } from "@fortawesome/free-regular-svg-icons";
+import { RESOURCES } from "@routine-management-system/constants";
+import { Timeline } from "@routine-management-system/lib";
+import { ResourceButton } from "./components";
+
+interface LessonDateTime {
+  day: string;
+  hours: string;
+  minutes: string;
+  month: string;
+  year: string;
+}
+
+interface LessonProps {
+  chapter: string;
+  datetime: LessonDateTime;
+  subject: string;
+  timeline: Timeline;
+}
+
+export function Lesson({
+  chapter = "চেপটার ১ - ভৌত বিজ্ঞান",
+  datetime = {
+    day: "৩",
+    hours: "২",
+    minutes: "৫৫",
+    month: "মে",
+    year: "২০২৫",
+  },
+  subject = "বাংলা ১ম পত্র",
+  timeline,
+}: LessonProps) {
+  return (
+    <div>
+      <div className="flex gap-3">
+        <div className="flex gap-2">
+          <FontAwesomeIcon icon={faCalendar} />
+          <div>
+            {datetime.day} {datetime.month}, {datetime.year}
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <FontAwesomeIcon icon={faClockFour} />
+          <div>
+            {datetime.hours} টা {datetime.minutes} মিনিট
+          </div>
+        </div>
+      </div>
+      <div
+        className={`border ${
+          timeline === "present" ? "border-green-g300" : "border-neutral-b40"
+        } flex flex-col gap-3 p-3 rounded-[12px]`}
+      >
+        <div className="flex flex-col gap-1">
+          <div className="font-semibold text-lg text-neutral-b700">
+            {subject}
+          </div>
+          <div className="font-normal text-base text-neutral-b300">
+            {chapter}
+          </div>
+        </div>
+        <div className="flex gap-4">
+          {RESOURCES.map((resource, index) => (
+            <ResourceButton
+              Icon={resource.icon}
+              isGhost={index === 2 && timeline === "past"}
+              key={index}
+              name={resource.name}
+              timeline={timeline}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,38 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+enum LessonDateTimeIconColor {
+  GREEN_G300 = "#008643",
+  NEUTRAL_B100 = "#757575",
+  NEUTRAL_B700 = "#1c1c1c",
+}
+
 export type Timeline = "past" | "present" | "future";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function determineLessonDateTimeColor(timeline: Timeline) {
+  switch (timeline) {
+    case "past":
+      return "text-neutral-b100";
+    case "present":
+      return "text-green-g300";
+    case "future":
+      return "text-neutral-b700";
+  }
+}
+
+export function determineLessonDateTimeIconColor(timeline: Timeline) {
+  switch (timeline) {
+    case "past":
+      return LessonDateTimeIconColor.NEUTRAL_B100;
+    case "present":
+      return LessonDateTimeIconColor.GREEN_G300;
+    case "future":
+      return LessonDateTimeIconColor.NEUTRAL_B700;
+  }
 }
 
 export function determineResourceButtonColors(
