e87b4b36e50372cf46b91da4389c3d34dad18876
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-05-28 15:36:22 +0600

    Add resource button component

---
+++ src/app/student/components/TimelineView/components/ResourceButton.tsx
@@ -0,0 +1,39 @@
+import {
+  ResourceIcon,
+  ResourceIconColor,
+} from "@routine-management-system/components/icons";
+import { Button } from "@routine-management-system/components/ui";
+import {
+  determineResourceButtonColors,
+  Timeline,
+} from "@routine-management-system/lib";
+
+interface ResourceButtonProps {
+  Icon: ResourceIcon;
+  isGhost: boolean;
+  name: string;
+  timeline: Timeline;
+}
+
+export function ResourceButton({
+  Icon,
+  isGhost,
+  name,
+  timeline,
+}: ResourceButtonProps) {
+  return (
+    <Button
+      className={`${determineResourceButtonColors(
+        isGhost,
+        timeline
+      )} flex gap-1 py-1 rounded-xl w-50.75`}
+    >
+      <Icon
+        color={
+          isGhost ? ResourceIconColor.NEUTRAL_B55 : ResourceIconColor.WHITE
+        }
+      />
+      <div className="font-normal text-sm">{name}</div>
+    </Button>
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export type Timeline = "past" | "present" | "future";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function determineResourceButtonColors(
+  isGhost: boolean,
+  timeline: Timeline
+) {
+  switch (timeline) {
+    case "past":
+      return isGhost
+        ? "bg-white border border-b40 text-b55"
+        : "bg-neutral-b300 border-none text-white";
+    case "present":
+      return "bg-green-g300 border-none text-white";
+    case "future":
+      return "bg-neutral-b700 border-none text-white";
+  }
 }
