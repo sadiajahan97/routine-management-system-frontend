3eff4bbeb50fe380d8b3997bcc4eecda2e43dd8d
Sadia Iffat Jahan <sadiaiffatjahan@gmail.com>
2025-05-30 10:45:27 +0600

    Add admin and student buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { Button } from "@routine-management-system/components/ui";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <main></main>;
+  const router = useRouter();
+  return (
+    <main className="flex flex-col gap-4">
+      <Button onClick={() => router.push("/admin")}>Admin</Button>
+      <Button onClick={() => router.push("/student")}>Student</Button>
+    </main>
+  );
 }
